e56ea0674f5b15b28313ffb9048082f93c319052
Alvin <alvinyongho@gmail.com>
2021-09-22 22:13:59 -0700

    fixes import

--- blackjack_gym/__init__.py
+++ blackjack_gym/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
   id='BlackjackEnv-v0',
-  entry_point='blackjack_gym.envs.blackjack_env:BlackjackEnv',
+  entry_point='blackjack_gym.envs:BlackjackEnv',
 )
